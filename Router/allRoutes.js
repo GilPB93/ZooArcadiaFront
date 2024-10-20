ceb57a381cae7e4004cd9b734ec6205f0677fa4f
GilPB93 <g.p.braga@hotmail.com>
2024-10-20 12:52:46 +0200

    Add connection page

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,7 +2,10 @@ import Route from "./Route.js";
 
 //Définir ici les routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),];
+    new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/signin", "Connexion", "/pages/signin.html"),
+
+];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Zoo Arcadia";
